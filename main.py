e173e9ca43509dee423277a93e4efaee80db90ce
Rain Ramm <rammrain@gmail.com>
2014-01-20 21:25:21 +0200

    Started on Pi to get temp from sensors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from Sender import *
 from Config import *
+import os
+
+os.system("sudo modprobe w1-gpio")
+os.system("sudo modprobe w1-therm")
 
 config = Config()
-
 sender = Sender(config)
 
-sender.send(12.175, 1)
+for dirname, dirnames, filenames in os.walk("/sys/bus/w1/devices"):
+    for subdirname in dirnames:
+        print(subdirname)
+
+
+
+#sender.send(12.175, 1)
