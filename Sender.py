ca7e374022b2ebbf3183b0ed4936e500edd2d52a
Rain Ramm <rammrain@gmail.com>
2015-06-08 11:04:46 +0300

    Implement new api.

--- Sender.py
+++ Sender.py
@@ -4,14 +4,10 @@ import requests
 class Sender:
     config = 0
 
-    def __init__(self, config):
-        self.config = config
-
     def send(self, temperature, sensor):
         request_params = {
-            'hash': self.config.apiHash,
             'sensor': sensor,
             'temperature': temperature
         }
 
-        return requests.get(self.config.requestUrl, params=request_params)
+        return requests.post('http://webser.ee:3000/temperature', request_params)
--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 
 from Sender import *
-from Config import *
 from Sensor import *
 import os
 
 os.system("sudo modprobe w1-gpio")
 os.system("sudo modprobe w1-therm")
 
-config = Config()
-sender = Sender(config)
+sender = Sender()
 
 for dirname, dirnames, filenames in os.walk("/sys/bus/w1/devices"):
     for subdirname in dirnames:
         if subdirname != "w1_bus_master1":
             sensor = Sensor(subdirname)
             temp = sensor.get_temp()
-            response = sender.send(temp, Config.sensorMap[subdirname])
+            response = sender.send(temp, subdirname)
 
             # debug
             print(temp)
